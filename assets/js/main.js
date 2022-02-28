3a3c52916bc2c12eaae751e6fe19f15b6c6bf1c7
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-03-01 00:01:28 +0600

    adding 20 products and make resopnsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,39 @@
-console.log('connected');
+// creating showAllProducts function
+const showAllProducts = () => {
+    fetch(`https://openapi.programming-hero.com/api/phones`)
+        .then(res => res.json())
+        .then(data => showingData(data.data))
+}
+
+// showing data function
+const showingData = (phones) => {
+    const productContainer = document.getElementById("products-row");
+    let i;
+    for (i = 0; i <= 20; i++) {
+        const {
+            brand,
+            image,
+            phone_name,
+            slug
+        } = phones[i];
+
+        // creating single card
+        const div = document.createElement('div');
+        div.setAttribute("class", "col-lg-4 col-md-6 col-sm-6 col-10 mb-4 rounded");
+        const element = `
+        <div class = "card py-4 shadow-sm">
+            <div class = "card-body">
+                <img class="d-block mx-auto img-fluid" src=${image} alt="phone-picture"/>
+                <h5 class = "card-title mt-4"> ${phone_name} </h5> 
+                <p class = "card-text text-muted"> Brand: <span> ${brand} </span>
+                </p>
+                <a href = "#" class = "btn btn-success"> See Details </a> 
+            </div>
+        </div>     
+        `;
+
+        div.innerHTML = element;
+        productContainer.appendChild(div);
+
+    }
+}
